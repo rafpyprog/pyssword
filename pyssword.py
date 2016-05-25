724415852a168a8ddfc435e418cb412ef7ecf4b3
Rafael Alves Ribeiro <rafael.alves.ribeiro@gmail.com>
2016-05-25 18:46:08 -0300

    Add the backspace action

--- pyssword.py
+++ pyssword.py
@@ -1,25 +1,36 @@
-'''Python tool to get a password from the user and display a masked
-   value at the prompt.
-'''
-# Author: Rafael Alves Ribeiro
-
 from msvcrt import getch
 import getpass, sys
+import time
 
 def pyssword(prompt='Password: '):
+    '''
+        Prompt for a password and masks the input.
+        Returns:
+            the value entered by the user.
+    '''
+    
     if sys.stdin is not sys.__stdin__:
         pwd = getpass.getpass(prompt)
         return pwd
     else:
-        sys.stdout.write(prompt)
         pwd = ""
+        print len(pwd)
+        countBackSpace = 0
+        sys.stdout.write(prompt)        
         while True:
             key = ord(getch())
-            if key == 13: #Enter
+            if key == 13: #Return Key
                 sys.stdout.write('\n')
                 return pwd
                 break
+            if key == 8: #Backspace key
+                if len(pwd) > 0:
+                    # Erases previous character.
+                    sys.stdout.write('\b' + ' ' + '\b')                
+                    sys.stdout.flush()
+                    pwd = pwd[:-1]                    
             else:
+                # Masks user input.
                 char = chr(key)
                 sys.stdout.write('*')                
-                pwd = pwd + char
+                pwd = pwd + char            
