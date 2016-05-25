996489d95b56788db46e02f0227c90cc8a838c26
Rafael Alves Ribeiro <rafael.alves.ribeiro@gmail.com>
2016-05-25 17:51:36 -0300

    Comments

--- pyssword.py
+++ pyssword.py
@@ -1,6 +1,10 @@
+'''Python tool to get a password from the user and display a masked
+   value at the prompt.
+'''
+# Author: Rafael Alves Ribeiro
+
 from msvcrt import getch
-import getpass
-import sys
+import getpass, sys
 
 def pyssword(prompt='Password: '):
     if sys.stdin is not sys.__stdin__:
